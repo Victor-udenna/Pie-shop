ca1f431cb5574b873afa2b1798b9812b69d4870d
victorudenna@gmail.com <victorudenna@gmail.com>
2022-08-11 03:14:47 +0100

    Edited layout

--- js/menuToggle.js
+++ js/menuToggle.js
@@ -1,6 +1,11 @@
 
 function toggleMenu() {
-    document.getElementById("primaryNav").classList.toggle("open");
+    document.getElementById('primaryNav').classList.toggle('open');
 }
+
+// creating the function and adding the hiding content
+
 let x = document.getElementById('hamburgerBtn');
-x.onclick = toggleMenu; 
+    x.onclick = toggleMenu;
+
+    // adding the click btn in the variable and calling the function that contains the content
